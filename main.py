13f55b0163c8a84a8a71c513ee810b16a0befbe6
Tyler Keith <55261569+keitht22@users.noreply.github.com>
2019-10-24 11:16:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     # creates directory for formatted images to be sent to
     # this option is more favorable than overwriting the images
     try:
-        if not os.path.isfile('/path/for/new/directory/name-of-directory-to-create'):
+        if not os.path.isfile('/Users/tylerkeith/Documents/Research/Destination for Formatted'):
             makeDestDir()
             imageResize()
     except FileExistsError:
@@ -16,8 +16,8 @@ def main():
 
 def makeDestDir():
 
-    directory = "name-of-directory-to-create"
-    path = "/path/for/new/directory"
+    directory = "Destination for Formatted"
+    path = "/Users/tylerkeith/Documents/Research"
     fullpath = os.path.join(path, directory)
     os.mkdir(fullpath)
 
@@ -26,19 +26,24 @@ def imageResize():
 
     # changes the working directory to the copied version
     # of our gathered data sets
-    dirlist = os.listdir('directory-with-images-to-resize')
+    dirlist = os.listdir('Data Sets for Testing copy 2')
 
     # iterates over each .png or .jpg image file in the directory
     for c, file in enumerate(dirlist):
-        if ('.png' in file) or ('.jpg' in file):
-            img = 'directory-with-images-to-resize' + os.path.sep + file
-            imgread = cv2.imread(img)
-            height, width = imgread.shape[0], imgread.shape[1]
+        if ('.png' in str(file)) or ('.jpg' in str(file)):
+            # we split original file names at the understore in order to isolate
+            # the observed number of clusters for comparison with our machine's prediction
+            tokens = file.split('_')
+            clustnum = int(tokens[1].split(".")[0])
+            filect = str(c) + '_' + str(clustnum)
+            print(clustnum)
+            pic = 'Data Sets for Testing copy' + os.path.sep + file
+            imgread = cv2.imread(pic)
 
             # the creation of our resized images
             # and the movement of said images to our destination directory
-            new_img = cv2.resize(imgread, (500, 500))
-            cv2.imwrite('/path/for/new/directory/name-of-directory-to-create' + str(c) + '.png', new_img)
+            img = cv2.resize(imgread, (500, 500))
+            cv2.imwrite('/Users/tylerkeith/Documents/Research/Destination for Formatted/' + filect + '.jpg', img)
 
 
 if __name__ == '__main__':
